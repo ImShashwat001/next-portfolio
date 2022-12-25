fcd67b06a1f8fc585c9808930a04f8ae05de9c3f
Shashwat <shashwatpanda4@gmail.com>
2022-12-25 08:59:25 +0530

    adding yesterday's build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Head>
         <title>My Portfolio </title>  
       </Head>
+      <Header />
     </>
   )
 }
