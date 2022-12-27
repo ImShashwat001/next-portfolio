a25e006d70e9379f98d853edcba548457d53c99d
Shashwat <shashwatpanda4@gmail.com>
2022-12-27 22:26:18 +0530

    27-12 updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 type Props = {}
 
 export default function({}: Props) {
   return (
-    <div>
-        Social Icons
+    <header>
+ 
+    <div className='flex flex-row items-center'>
+    <SocialIcon url="https://www.youtube.com/sonnysangha"
+    fgColor='gray'
+    bgColor='transparent'
+    />
+    <SocialIcon url="https://www.youtube.com/sonnysangha"
+    fgColor='gray'
+    bgColor='transparent'
+    />
+    <SocialIcon url="https://www.youtube.com/sonnysangha"
+    fgColor='gray'
+    bgColor='transparent'
+    />
     </div>
+    <div>
+      <SocialIcon
+        className="cursor-pointer"
+        network="email"
+        fgColor="gray"
+        bgColor="transparent"
+      />
+      <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in Touch</p>
+    </div>
+
+    </header>
   )
 }
