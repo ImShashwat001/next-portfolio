3d4a45927738fdb7634e612691055f52f823a675
Shashwat <shashwatpanda4@gmail.com>
2023-01-06 11:15:56 +0530

    skills names added

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>My Portfolio </title>  
       </Head>
       <Header />
-      <section id='hero'>
+      <section id='hero' className='snap-center'>
         <Hero />
       </section>
     </div>
