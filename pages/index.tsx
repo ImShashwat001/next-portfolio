a5ca7c8da23ac1f7089add6fa2d9d6ae59d7e47a
Shashwat <shashwatpanda4@gmail.com>
2023-01-08 11:03:20 +0530

    added components in global css

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,10 @@ export default function Home() {
       <Header />
       <section id='hero' className='snap-center'>
         <Hero />
+      </section>
+
+      <section>
+          <About />
       </section>
     </div>
   )
