2c11fb772e6bd65264fc8a649b6f3be6a8416789
Shashwat <shashwatpanda4@gmail.com>
2023-01-11 02:56:55 +0530

    about section done

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,12 @@ type Props = {}
 
 function About({}: Props) {
   return (
-    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <motion.div
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 1.5}}
+      
+    className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
 
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
       <motion.div
@@ -33,7 +38,7 @@ function About({}: Props) {
           <h4 className='text-4xl font-semibold'>Here is a <span className='underline decoration-[#F7AB0A/50]'>little</span> background</h4>
           <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo vel accusamus repudiandae quas nulla, ipsam neque beatae officiis accusantium ducimus omnis eos facilis, porro odio inventore. Voluptas nostrum dolores maxime.</p>
         </div>
-      </div>
+      </motion.div>
 
   )
 }
