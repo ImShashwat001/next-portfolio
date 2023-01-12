c809599c105e8071ff4edd300366aaadb23ae4b4
Shashwat <shashwatpanda4@gmail.com>
2023-01-12 13:14:36 +0530

    setting up experience

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Image from "next/image"
 import ProImg from '../public/adria.jpg'
 import { motion } from "framer-motion"
+import typeOne from '../public/react.png'
+import typeTwo from '../public/js.png'
+import typeThree from '../public/firebase.png'
+import typeFour from '../public/nodejs.png'
 
 type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <article>
+    <article className='flex flex-col rounded-l items-center'>
         <motion.div
             initial={{
                 y: -100,
@@ -28,15 +32,14 @@ function ExperienceCard({}: Props) {
           <h4 className='text-4xl font-light'>CEO of PAPAFAM</h4>
           <p className='font-bold text-2xl mt-1'>PAPAFAM</p>
           <div className='flex space-x-2my-2'>
-
-            {/* Tech used */}
-            {/* Tech used */}
-            {/* Tech used */}
-            {/* Tech used */}
+            <Image className='h-10 w-10 rounded-full' src={typeOne} alt='react logo' />
+            <Image className='h-10 w-10 rounded-full' src={typeTwo} alt='javascript logo' />
+            <Image className='h-10 w-10 rounded-full' src={typeThree} alt='firebase logo' />
+            <Image className='h-10 w-10 rounded-full' src={typeFour} alt='nodejs logo' />
 
 
           </div>
-            <p>Started work... -Ended...</p>
+            <p className='uppercase py-5 text-gray-300'>Started work... -Ended...</p>
             <ul className='list-disc space-y-4 ml-5 text-lg'>
               <li>Summary Points</li>
               <li>Summary Points</li>
