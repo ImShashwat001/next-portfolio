f07deccaebeae3342cabf0d05184d34f5a1a0257
Shashwat <shashwatpanda4@gmail.com>
2023-01-13 13:46:57 +0530

    adding CSS to experience cards

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -11,7 +11,7 @@ type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <article className='flex flex-col rounded-l items-center'>
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] lg:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
         <motion.div
             initial={{
                 y: -100,
