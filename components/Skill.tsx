88afd5f8d1753180ac64cfa2df9971085f92c468
Shashwat <shashwatpanda4@gmail.com>
2023-01-14 09:39:47 +0530

    setting some css in skill component

---
+++ components/Skill.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { motion } from "framer-motion"
+
+type Props = {}
+
+function Skill({}: Props) {
+  return (
+    <motion.div className=''>
+      <h3>
+        skills
+      </h3>
+
+    </motion.div>
+  )
+}
+
+export default Skill
