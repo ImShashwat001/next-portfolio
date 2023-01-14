b94fb80e539a92f0f3bb7465d10e30830b8a1632
Shashwat <shashwatpanda4@gmail.com>
2023-01-14 21:45:33 +0530

    upgraded skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,16 +1,36 @@
 import React from 'react'
 import { motion } from "framer-motion"
+import Image from 'next/image'
+import ProImg from '../public/adria.jpg'
 
-type Props = {}
+type Props = {
+  directionLeft?: boolean;
+}
 
-function Skill({}: Props) {
+function Skill({ directionLeft }: Props) {
   return (
-    <motion.div className=''>
-      <h3>
-        skills
-      </h3>
-
-    </motion.div>
+    <div className='group relative flex cursor-pointer'>
+      <motion.div
+      className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        initial={{
+          x: directionLeft ? -200 : 200,
+          opacity: 0
+        }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0 }}
+      >
+        <Image 
+          
+          src={ProImg}
+          alt='skill image'
+        />
+      </motion.div>
+        <div>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+          </div>
+        </div>
+    </div>
   )
 }
 
