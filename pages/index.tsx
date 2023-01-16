8446881b34bb0ffdb4c5cea2350b99ac5dfd0809
Shashwat <shashwatpanda4@gmail.com>
2023-01-16 19:24:58 +0530

    project.tsx building

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,6 +29,9 @@ export default function Home() {
       </section>
       <section id='skills' className='snap-start'>
           <Skills />
+      </section>
+      <section id='projects' className='snap-start'>
+        <Projects/>
       </section>
     </div>
   )
