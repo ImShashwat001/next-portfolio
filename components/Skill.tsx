e72e73d0b231eb6694c67b405c5b01ba19a8cb5f
Shashwat <shashwatpanda4@gmail.com>
2023-01-16 11:30:23 +0530

    skill css completed

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -25,7 +25,7 @@ function Skill({ directionLeft }: Props) {
           alt='skill image'
         />
       </motion.div>
-        <div>
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
           <div className='flex items-center justify-center h-full'>
             <p className='text-3xl font-bold text-black opacity-100'>100%</p>
           </div>
