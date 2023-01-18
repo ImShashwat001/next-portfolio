baeed09eb1a83638c51affc778e813e28bffd258
Shashwat <shashwatpanda4@gmail.com>
2023-01-18 14:20:35 +0530

    further css update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Image from 'next/image'
 import ProImg from '../public/adria.jpg'
+import { motion } from "framer-motion"
 
 type Props = {}
 
 function Projects({}: Props) {
   const projects = [1, 2, 3, 4, 5]
   return (
-    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
+    <motion.div
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+    
+    className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
 
       <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
@@ -15,11 +21,22 @@ function Projects({}: Props) {
         {projects.map((project, i) => (
           <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
             
+            <motion.div
+              initial={{
+                y: -300,
+                opacity: 0
+              }}
+              transition={{ duration: 1.2 }}
+              whileInView={{ opacity: 1, y: 0
+              }}
+              viewport={{ once: true }}
+            >
             <Image 
               src={ProImg}
               alt="project photo"
               className='w-20 h-20'
             />
+            </motion.div>
             
             
             <div className='space-y-10 px-10 md:px-10 max-w-6xl'>
@@ -33,7 +50,7 @@ function Projects({}: Props) {
       </div>
 
       <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12' />
-    </div>
+    </motion.div>
   )
 }
 
