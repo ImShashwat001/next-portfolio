3694dde68af2a26b54b8f5162066c7826ea43981
Shashwat <shashwatpanda4@gmail.com>
2023-01-21 22:42:23 +0530

    contactMe component added

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
+import ContactMe from '../components/ContactMe'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,6 +33,9 @@ export default function Home() {
       </section>
       <section id='projects' className='snap-start'>
         <Projects/>
+      </section>
+      <section id='' className=''>
+          <ContactMe />
       </section>
     </div>
   )
