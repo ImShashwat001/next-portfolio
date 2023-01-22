fdc352a196746359e649e86bb1e83aebeaa82122
Shashwat <shashwatpanda4@gmail.com>
2023-01-22 14:55:16 +0530

    titlemade center

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -4,7 +4,13 @@ type Props = {}
 
 function ContactMe({}: Props) {
   return (
-    <div>ContactMe</div>
+    <div className='h-screen flex relative flex-col text-center md:flex-row max-w-7xl px-18 justify-evenly mx-auto items-center'>
+      
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+        Contact
+      </h3>
+    
+    </div>
   )
 }
 
